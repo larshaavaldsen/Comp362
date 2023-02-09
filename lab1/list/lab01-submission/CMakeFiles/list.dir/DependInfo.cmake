1f3a36a6b42ed8e637b39aaa793dc13c55a65698
lars <marzshine@protonmail.com>
2023-02-09 20:24:23 +0000

    Lab3 setup

---
+++ lab1/list/lab01-submission/CMakeFiles/list.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lars/comp362/lab1/list/lab01-submission/list.c" "CMakeFiles/list.dir/list.c.o" "gcc" "CMakeFiles/list.dir/list.c.o.d"
+  "/home/lars/comp362/lab1/list/lab01-submission/main.c" "CMakeFiles/list.dir/main.c.o" "gcc" "CMakeFiles/list.dir/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab3/task1/CMakeFiles/memory_layout.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/memory_layout.dir/memory_layout.c.o"
+  "CMakeFiles/memory_layout.dir/memory_layout.c.o.d"
+  "bin/memory_layout"
+  "bin/memory_layout.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/memory_layout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
