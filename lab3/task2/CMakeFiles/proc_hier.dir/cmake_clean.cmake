7086d5bf3c8b8c91f398348e291e93cb73a9a194
lars <marzshine@protonmail.com>
2023-02-14 18:32:25 +0000

    Update

---
+++ lab3/task2/CMakeFiles/proc_hier.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/proc_hier.dir/proc_hier.c.o"
+  "CMakeFiles/proc_hier.dir/proc_hier.c.o.d"
+  "bin/proc_hier"
+  "bin/proc_hier.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/proc_hier.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab3/task2/CMakeFiles/proc_hier.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for proc_hier.
+# This may be replaced when dependencies are built.
